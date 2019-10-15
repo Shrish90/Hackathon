7333211b60c675a3ba3a4550c5604566527c43e1
shrish90 <shrish.tiwari90@gmail.com>
2019-10-15 11:37:17 +0000

    loan dao

--- hackathon/src/main/java/com/hackathon/DAO/DAOImpl/LoanDAOImpl.java
+++ hackathon/src/main/java/com/hackathon/DAO/DAOImpl/LoanDAOImpl.java
@@ -19,6 +19,8 @@ public class LoanDAOImpl implements LoanDAO{
 	private final String loan_persist_query = "INSERT INTO loandetails(loantype, amount, rate, cid, userid) VALUES (?,?,?,?,?)";
 	private final String get_userid_query = "SELECT userid FROM userdetails where passport = ?";
 	private final String get_companyid_query = "SELECT cid FROM companydetails where cregisterno = ?";
+	private final String get_amount_by_cid = "SELECT l.amount FROM loandetails l,companydetails c where c.cid = l.cid and c.cregisterno = ?";
+	private final String get_amount_by_userid = "SELECT l.amount FROM loandetails l,userdetails u where l.userid = u.userid and u.passport = ?";
 	private final int rateOfInterest = 13;
 	@Override
 	public boolean persistLoan(LoanApplication loanApplication) {
@@ -38,6 +40,28 @@ public class LoanDAOImpl implements LoanDAO{
 	public int getCompanyIdByRegNo(String regNo) {
 		List<Map<String,Object>> cId = jdbcTemplate.queryForList(get_companyid_query, regNo);
 		return Integer.parseInt(cId.get(0).get("cid")+"");	
+	}
+
+	@Override
+	public double getLoanAmountByRegNo(String regNo) {
+		double result = 0.0;
+		List<Map<String,Object>> amounts = jdbcTemplate.queryForList(get_amount_by_cid, regNo);
+		for(Map<String,Object> amountObj:amounts) {
+			result = result + Double.parseDouble(amountObj.get("amount")+"");
+		}
+		logger.info("total loan on company" + result);
+		return result;
+	}
+
+	@Override
+	public double getLoanAmountByPassport(String passport) {
+		double result = 0.0;
+		List<Map<String,Object>> amounts = jdbcTemplate.queryForList(get_amount_by_userid, passport);
+		for(Map<String,Object> amountObj:amounts) {
+			result = result + Double.parseDouble(amountObj.get("amount")+"");
+		}
+		logger.info("total loan on user" + result);
+		return result;
 	}
 }
 	

c0b846af026ccdaff696137d506f035dbcaafc02
shrish90 <shrish.tiwari90@gmail.com>
2019-10-15 07:08:14 +0000

    component scan added

--- hackathon/src/main/java/com/hackathon/application/HackathonApplication.java
+++ hackathon/src/main/java/com/hackathon/application/HackathonApplication.java
@@ -3,7 +3,7 @@ package com.hackathon.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hackathon.*"})
 public class HackathonApplication {
 
 	public static void main(String[] args) {
--- hackathon/src/main/java/com/hackathon/controllers/ApplyLoanController.java
+++ hackathon/src/main/java/com/hackathon/controllers/ApplyLoanController.java
@@ -1,5 +1,47 @@
 package com.hackathon.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+* <h1>Apply Loan Controller </h1>
+* This is the Controller to take the loan details from UI
+* and send the status back as success or failure
+*
+* @author  Shrish Tiwari
+* @version 1.0
+* @since   2019-10-15
+*/
+@RestController
+@RequestMapping("/ApplyLoan")
+@CrossOrigin("*")
 public class ApplyLoanController {
 
+private static final Logger logger = LoggerFactory.getLogger(ApplyLoanController.class);
+
+
+/**
+ * This method is used to collect Account details coming from UI
+ * and verify for loan amount if eligible
+ * @param 
+ * @return 
+ */
+
+@RequestMapping(value = "/verify")
+public ResponseEntity<Map<String,String>> ApplyLoan(){
+	Map<String,String> result = new HashMap<>();
+	logger.info("****RUNNING*****");
+	result.put("root", "working fine");
+	return new ResponseEntity<>(result, HttpStatus.OK);
 }
+
+}
+

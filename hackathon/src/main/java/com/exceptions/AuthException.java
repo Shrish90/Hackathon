2b78008b1ca9a64841c3dcbe49675b4683e88c6c
AVK Kiran Kumar <avk.kirankumar@gmail.com>
2019-10-15 08:40:43 +0000

    adding comments
    adding comments

--- hackathon/src/main/java/com/exceptions/AuthException.java
+++ hackathon/src/main/java/com/exceptions/AuthException.java
@@ -1,5 +1,10 @@
 package com.exceptions;
 
+/**
+ * This is the exception that is thrown when login fails.
+ * @author kiranavk
+ *
+ */
 public class AuthException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;

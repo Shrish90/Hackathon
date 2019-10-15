2e0b3f1ef9ac7391739eae6976752a4754d60510
shrish90 <shrish.tiwari90@gmail.com>
2019-10-15 12:01:36 +0000

    dateofbirth calculation

--- hackathon/src/main/java/com/hackathon/service/serviceImpl/LoanServiceImpl.java
+++ hackathon/src/main/java/com/hackathon/service/serviceImpl/LoanServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hackathon.service.serviceImpl;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Calendar;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -49,8 +52,6 @@ public class LoanServiceImpl implements LoanService{
 		}
 		if(isEligible) {
 			loanDao.persistLoan(loanApplication);
-		}else {
-			
 		}
 		return isEligible;
 	}
@@ -63,8 +64,15 @@ public class LoanServiceImpl implements LoanService{
 			return loanDao.getLoanAmountByPassport(passport);
 		}
 	public int calculateAgeByDob(Date dob) {
-		
-		return 33;
+		  Calendar c = Calendar.getInstance();
+		  c.setTime(dob);
+		  int year = c.get(Calendar.YEAR);
+		  int month = c.get(Calendar.MONTH) + 1;
+		  int date = c.get(Calendar.DATE);
+		  LocalDate l1 = LocalDate.of(year, month, date);
+		  LocalDate now1 = LocalDate.now();
+		  Period diff1 = Period.between(l1, now1);
+		  return diff1.getYears();
 	}
 	
 }
